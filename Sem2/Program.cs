6472a4c2926c25350467644aeff0114bc90b6632
bayushidzen <zhek_wh@mail.ru>
2023-10-14 16:50:21 +0300

    HW2 create menu, update version number

--- Sem2/Program.cs
+++ Sem2/Program.cs
@@ -4,11 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Выравниватель массивов v 0.01");
+            Console.WriteLine("Выравниватель массивов v 0.02");
             Console.WriteLine("Исходный массив выглядит так: ");
             Console.WriteLine(" 7, 3, 2 ");
             Console.WriteLine(" 4, 9, 6 ");
             Console.WriteLine(" 1, 8, 5 ");
+
+            Console.WriteLine("{0,15}", "МЕНЮ");
+            Console.WriteLine("Как вы хотите создать массив?");
+            Console.WriteLine("{0,10}", "1) Вручную 2) Автоматически");
+            int.TryParse(Console.ReadLine(), out int number);
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Результат выполнения операции 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Результат выполнения операции 2");
+                    break;
+                default:
+                    Console.WriteLine("Для использования программы выберите режим ручной или автоматический");
+                    break;
+            }
+
 
             int[,] arr = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
             int y = arr.GetLength(0);
