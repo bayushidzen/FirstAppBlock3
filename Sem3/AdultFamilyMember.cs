72a367828a47e13470d3d50ac40e02d51e322dab
bayushidzen <zhek_wh@mail.ru>
2023-10-14 19:30:31 +0300

    HW3 make template

---
+++ Sem3/AdultFamilyMember.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem3
+{
+    public class AdultFamilyMember : FamilyMember
+    {
+        public FamilyMember[] Children { get; set; }
+        public override void Print(int indent = 0)
+        {
+            base.Print(indent);
+
+            foreach (var child in Children)
+                child.Print(indent * 2);
+        }
+    }
+}
---
+++ Sem3/FamilyMember.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem3
+{
+    public class FamilyMember
+    {
+        public FamilyMember Mother { get; set; }
+        public FamilyMember Father { get; set; }
+        public string Name { get; set; }
+        public Gender Sex { get; set; }
+
+        void Info(int indent = 0)
+        {
+            Console.WriteLine($"{new String('-', indent)}Имя {this.Name}");
+        }
+        public virtual void Print(int indent = 0)
+        {
+            Info(indent);
+        }
+    }
+}
