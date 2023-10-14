fad8ed0e8c7067d193bc0273be291bec7f87fde7
bayushidzen <zhek_wh@mail.ru>
2023-10-14 18:53:54 +0300

    HW2 create manaul&autogeneretion array classes, create print and sort array classes, updated menu

---
+++ Sem2/Generator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem2
+{
+    internal class Generator
+    {
+        public static int[,] AutoGenerator() {
+
+            Console.WriteLine("Укажите размерность массива: ");
+            int.TryParse(Console.ReadLine(), out int arrSize);
+            int[,] arrAuto = new int[arrSize, arrSize];
+
+            for (int i = 0; i < arrSize; i++)
+            {
+                for (int j = 0; j < arrSize; j++)
+                {
+                    Console.Write(" " + arrAuto[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            return arrAuto;
+        }
+    }
+}
--- Sem2/Program.cs
+++ Sem2/Program.cs
@@ -1,36 +1,86 @@
-﻿namespace Sem2
+﻿using Sem2;
+using System.Drawing;
+
+namespace Sem2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Выравниватель массивов v 0.02");
-            Console.WriteLine("Исходный массив выглядит так: ");
-            Console.WriteLine(" 7, 3, 2 ");
-            Console.WriteLine(" 4, 9, 6 ");
-            Console.WriteLine(" 1, 8, 5 ");
+            Console.WriteLine("Выравниватель массивов v 0.03");
+
 
             Console.WriteLine("{0,15}", "МЕНЮ");
             Console.WriteLine("Как вы хотите создать массив?");
             Console.WriteLine("{0,10}", "1) Вручную 2) Автоматически");
             int.TryParse(Console.ReadLine(), out int number);
+
+            Console.WriteLine("Укажите размерность массива: ");
+            int.TryParse(Console.ReadLine(), out int arrSize);
+            int[,] arr = new int[arrSize, arrSize];
             switch (number)
             {
                 case 1:
-                    Console.WriteLine("Результат выполнения операции 1");
+                    Console.Clear();
+                    ManualGenerator(arrSize, arr);
+                    Console.WriteLine("Вы ввели такой массив: ");
+                    PrinArray(arr);
                     break;
                 case 2:
-                    Console.WriteLine("Результат выполнения операции 2");
+                    Console.Clear();
+                    AutoGenerator(arrSize, arr);
+                    Console.WriteLine("Автоматически сгенерирован массив: ");
+                    PrinArray(arr);
                     break;
                 default:
                     Console.WriteLine("Для использования программы выберите режим ручной или автоматический");
                     break;
             }
 
-
-            int[,] arr = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+            Console.WriteLine(" ");
+            Console.WriteLine("Результат сортировки: ");
+            SortArray(arr);
+            PrinArray(arr);
+        }
+        public static void ManualGenerator(int size, int[,] arr)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.WriteLine("Введите элемент массива и нажмите Enter: ");
+                    int.TryParse(Console.ReadLine(), out int arrMember);
+                    arr[i, j] = arrMember;
+                }
+            }
+        }
+        public static void AutoGenerator(int size, int[,] arr)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    arr[i, j] = new Random().Next(0, 101);
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void PrinArray(int[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(" " + arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void SortArray(int[,] arr)
+        {
             int y = arr.GetLength(0);
             int x = arr.GetLength(1);
+
             int[] sort = new int[x * y];
             int counter = 0;
             for (int i = 0; i < y; i++)
@@ -41,20 +91,15 @@
                     counter++;
                 }
             }
-
             Array.Sort(sort);
             counter = 0;
-            Console.WriteLine(" ");
-            Console.WriteLine("Результат сортировки: ");
             for (int i = 0; i < y; i++)
             {
                 for (int j = 0; j < x; j++)
                 {
                     arr[i, j] = sort[counter];
                     counter++;
-                    Console.Write(" " + arr[i, j] + " ");
                 }
-                Console.WriteLine();
             }
         }
     }
