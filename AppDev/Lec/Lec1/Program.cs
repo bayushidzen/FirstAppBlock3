4bcfbde100e0af809b3f153e9ddcc57d160a6199
bayushidzen <zhek_wh@mail.ru>
2023-12-06 20:23:47 +0300

    HW9

--- AppDev/Lec/Lec1/Program.cs
+++ AppDev/Lec/Lec1/Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
+        class Person
+        {
+            public string Name = String.Empty;
+            public DateTime Birthday;
+            public int Height;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Person person = new Person();
+            person.Name = "Vasya";
+            person.Birthday = DateTime.Parse("01.05.2000");
+            person.Height = 190;
+
+            Console.WriteLine($"{person.Name},{person.Birthday}, {person.Height}");
         }
     }
+
+
 }
---
+++ HW9/Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.Xml.Linq;
+
+namespace HW9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = "{\"VDD\": 44,\"REAL\": 0,\"Perem\": 1,\"ID\": \"_6QI0HB878014\",\"ND\": \"31426\",\"DD\": \"2023-10-16T00:00:00\",\"ORGANIZ\": 1,\"ORG_1C\": \"000002575\",\"CM_1C\": \"00000057345\"}";
+
+            var jsonObj = JsonConvert.DeserializeObject(json) as XObject;
+
+            XDocument xmlDoc = new XDocument(new XElement("Root", jsonObj.Properties().Select(p => new XElement(p.Name, p.Value))));
+            xmlDoc.Save("output.xml");
+        }
+    }
+}
