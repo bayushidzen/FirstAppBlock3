5a4d9e291572a36e9c4a82ceb591fbe13183827d
Bayushi Dzen <zhek_wh@mail.ru>
2023-12-19 20:09:50 +0300

    Lec1NetApp client and server first steps

---
+++ NetApp/Lec/Lec1Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace Lec1Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                var remoteEndPoint = new IPEndPoint(System.Net.IPAddress.Parse("127.0.0.1"), 12345);
+                Console.WriteLine("Connecting");
+                client.Connect(remoteEndPoint);
+                Console.WriteLine("Connected");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ NetApp/Lec/Sem1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Lec1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                var localEndPoint = new IPEndPoint(System.Net.IPAddress.Parse("127.0.0.1"), 12345);
+                listener.Blocking = true;
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
+                Console.WriteLine("Waiting for connection");
+                var socketListener = listener.Accept();
+                Console.WriteLine("Connected");
+                listener.Close();
+            }
+        }
+    }
+}
